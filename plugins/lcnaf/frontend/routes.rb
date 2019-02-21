fd773392d7797b42e575cec72c1981b5e621aa89
Steven D. Shelton <Steven-Shelton@UTC.edu>
2019-02-21 17:13:41 -0500

    removed all directories but local in plugins

--- plugins/lcnaf/frontend/routes.rb
+++
@@ -1,7 +0,0 @@
-ArchivesSpace::Application.routes.draw do
-  scope AppConfig[:frontend_proxy_prefix] do
-    match('/plugins/lcnaf' => 'lcnaf#index', :via => [:get])
-    match('/plugins/lcnaf/search' => 'lcnaf#search', :via => [:get])
-    match('/plugins/lcnaf/import' => 'lcnaf#import', :via => [:post])
-  end
-end
