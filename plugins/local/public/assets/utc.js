36171b9783a1e594291e1278d877999d9fed6215
Steven D. Shelton <Steven-Shelton@UTC.edu>
2019-06-04 14:23:47 -0400

    fixes for migrated server at findingaids/test.utc.edu

--- plugins/local/public/assets/utc.js
+++ plugins/local/public/assets/utc.js
@@ -1,30 +1,27 @@
-$(document).ready(function() {
-  $(function() {
-    $("#date").datepicker();
-  });
+$(document).ready(function(){
+//var $j = jQuery.noConflict();
+//  $j( function() {
+	jQuery(".datepicker").datepicker();
+//  } );
   // add home button to menu
-  $('#navigation ul').prepend("<li><a href='/'>Home</a></li>");
-  $(function() {
+  jQuery('#navigation ul').prepend("<li><a href='/'>Home</a></li>");
+  jQuery(function() {
     //get full url
     var url = location.href;
     // add envrironmental indicator
-    if (url.indexOf('archivesspace.lib.utc.edu') === -1) {
+
+    console.log(url);
+    if (url.indexOf('?findingaids.utc.edu?') === -1){
       $('body').prepend("<div id='dev-environment' class='alert alert-info' role='alert' style='padding: 0.25em; text-align: center; margin-bottom: 0px; display: block;'> | <strong>DEV</strong> environment | </div>");
     }
+
     // remove domain
     current = url.replace(/^.*\/\/[^\/]+/, '')
     //change title on home page
     if (current === "/") {
       document.title = "Finding Aids | Special Collections | University of Tennessee at Chattanooga";
     }
-    //capture Request click for possible redirect to form
-    /*
-    $('.request').click(function() {
-      alert("DEMO\nHandler for request button click called from:\n" + current + ". \nNew window will open to 'SPC -make an appt.' with appended refer. This would require work on the form to capture this information.");
-      window.open('https://www.utc.edu/library/special-collections/services/make-an-appointment.php?refer='+ current)
-    });
-    */
-    $('.top-bar ul li a').each(function() {
+    jQuery('.top-bar ul li a').each(function() {
       var $this = $(this);
       // if the current path is like this link, make it active
       if ($this.attr('href') === current) {
@@ -32,5 +29,5 @@ $(document).ready(function() {
       }
     })
   })
-  $('body').prepend('<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-TS9WVQ" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>');
+jQuery('body').prepend('<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-TS9WVQ" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>');
 });
