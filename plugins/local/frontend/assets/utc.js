ca912d18d3d82ce7430b1fa143d18a74d5879c94
Steven D. Shelton <Steven-Shelton@UTC.edu>
2019-07-09 15:52:43 -0400

    fixed environmental indicator on DEV + cleanup

--- plugins/local/frontend/assets/utc.js
+++ plugins/local/frontend/assets/utc.js
@@ -1,26 +1,10 @@
-$(document).ready(function(){
-  // add home button to menu
-  $('#navigation ul').prepend("<li><a href='/'>Home!</a></li>");
-  $(function() {
-    //get full url
-    var url = location.href;
-    console.log(url);
-    // add envrironmental indicator
-    if (url.indexOf('findingaidsstaff.utc.edu') === -1){
-      $('body').prepend("<div id='dev-environment' class='alert alert-info' role='alert' style='padding: 0.25em; text-align: center; margin-bottom: 0px; display: block;'> | <strong>DEV</strong> environment | </div>");
-    }
-    // remove domain
-    current = url.replace(/^.*\/\/[^\/]+/, '')
-    //change title on home page
-    if (current === "/") {
-      document.title = "Finding Aids | Special Collections | University of Tennessee at Chattanooga";
-    }
-    $('.top-bar ul li a').each(function() {
-      var $this = $(this);
-      // if the current path is like this link, make it active
-      if ($this.attr('href') === current) {
-        $this.addClass('active');
-      }
-    })
-  })
+$(document).ready(function() {
+//get full url
+var url = location.href;
+// add envrironmental indicator
+//console.log(url);
+if (url.indexOf('?findingaidsstaff.utc.edu?') === -1) {
+  $('body').prepend("<div id='dev-environment' class='alert alert-info' role='alert' style='padding: 0.25em; text-align: center; margin-bottom: 0px; display: block;'> | <strong>DEV</strong> environment | </div>");
+  }
+$('body').prepend('<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-TS9WVQ" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>');
 });
